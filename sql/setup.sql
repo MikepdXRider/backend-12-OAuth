31e934cd074f7be9c5c2adf102fe8a6792faba51
Michael Rider <mikepdxrider@gmail.com>
2022-02-10 13:02:35 -0800

    adds posts table - curr stack [api/v1/posts entry, / controller, getAll model]

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts;
 
 
 CREATE TABLE users(
@@ -8,4 +9,13 @@ CREATE TABLE users(
     username TEXT NOT NULL,
     email TEXT NOT NULL,
     avatar TEXT NOT NULL
-)
+);
+
+CREATE TABLE posts(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    user_id BIGINT NOT NULL REFERENCES(users.id),
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    category TEXT NOT NULL,
+    create_at TIMESTAMP WITH TIMEZONE DEFAULT CURRENT_TIMESTAMP
+);
